d5012e81da8d6e5c661b6dd421e26b7c233e6807
Ghislain Bourgeois <ghislain.bourgeois@bell.ca>
2016-08-23 11:59:19 -0400

    Add gitconfig

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ ln -sf ~/Dotfiles/.bashrc ~/.bashrc
 ln -sf ~/Dotfiles/.vimrc ~/.vimrc
 ln -sf ~/Dotfiles/.vim ~/.vim
 ln -sf ~/Dotfiles/.tmux.conf ~/.tmux.conf
-
+ln -sf ~/Dotfiles/.gitconfig ~/.gitconfig
