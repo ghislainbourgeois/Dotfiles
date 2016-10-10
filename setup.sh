7da2bfb86218d2169c448dd319df595268c8c3bc
Ghislain Bourgeois <ghislain.bourgeois@bell.ca>
2016-10-10 00:33:23 -0400

    Add basic i3 configuration.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-git_dir=$1
-home=$2
+git_dir=~/Dotfiles
+home=~/
 
 ln -sf $git_dir/.bashrc $home/.bashrc
 ln -sf $git_dir/.tmux.conf $home/.tmux.conf
 ln -sf $git_dir/.vimrc $home/.vimrc
 ln -sf $git_dir/.vim $home/.vim
+ln -sf $git_dit/.i3 $home/.i3
  
