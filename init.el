7df3f8b3c805608eb6fc2582e023f6276c878fdc
Ghislain Bourgeois <ghislain.bourgeois@gmail.com>
2017-10-20 15:02:02 -0400

    Add monokai theme

--- init.el
+++ init.el
@@ -1,16 +1,21 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
-(unless (package-installed-p 'use-package)
+
+(eval-when-compile
+  (require 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
-(require 'use-package)
 
 (use-package evil
   :ensure t
   :config
   (evil-mode 1))
+
+(use-package monokai-theme
+  :ensure t
+  :config
+  (load-theme 'monokai t))
 
 (setq inhibit-splash-screen t
       inhibit-startup-message t
@@ -24,7 +29,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (evil use-package))))
+ '(package-selected-packages (quote (use-package monokai-theme evil color-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
