3dbb9d1eed1c8efeb99448680d03243f73325902
Ghislain Bourgeois <ghislain.bourgeois@gmail.com>
2023-06-16 14:48:53 -0400

    Update automatic lock

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xss-lock --transfer-sleep-lock -- i3lock --nofork -i /home/ghislain/Pictures/Wallpapers/board.png &
+xss-lock --transfer-sleep-lock -- i3lock --nofork -i /home/ghislain/Pictures/wallpapers/seamless-2033661_640.png -t &
 ~/.fehbg &
 picom &
 nm-applet &
