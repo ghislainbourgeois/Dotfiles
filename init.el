7cf71c0b9544c6f8d8c2645803bd14b07ef1f96e
Ghislain Bourgeois <ghislain.bourgeois@canonical.com>
2024-03-02 15:46:15 -0500

    Set basic emacs configuration with theme and evil mode

--- init.el
+++ init.el
@@ -1,3 +1,11 @@
+(setq inhibit-splash-screen t
+      inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(global-display-line-numbers-mode 1)
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
@@ -12,24 +20,19 @@
   :config
   (evil-mode 1))
 
-(use-package monokai-theme
+(use-package gruvbox-theme
   :ensure t
   :config
-  (load-theme 'monokai t))
-
-(setq inhibit-splash-screen t
-      inhibit-startup-message t
-      inhibit-startup-echo-area-message t)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
+  (load-theme 'gruvbox-dark-medium t))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (use-package monokai-theme evil color-theme))))
+ '(custom-safe-themes
+   '("37c8c2817010e59734fe1f9302a7e6a2b5e8cc648cf6a6cc8b85f3bf17fececf" default))
+ '(package-selected-packages '(gruvbox-theme use-package monokai-theme evil color-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
